e5cf804237d245aa797935baad465577439c898d
kungmo <kungmo@gmail.com>
2020-09-17 10:26:33 +0900

    챗봇 업데이트 11 (챗봇 운영 datalog.xlsx 업데이트 20200917)

--- git_update.sh
+++ git_update.sh
@@ -3,5 +3,5 @@
 echo "git 업데이트"
 
 git add .
-git commit -m "챗봇 업데이트 11 (입출력 버그 고친 후 챗봇 운영 datalog.xlsx 업데이트 20200916)"
+git commit -m "챗봇 업데이트 11 (챗봇 운영 datalog.xlsx 업데이트 20200917)"
 git push origin master
