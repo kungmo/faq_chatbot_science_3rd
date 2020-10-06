ed934786e948d893828b94be1c9933cc10d0ebd4
kungmo <kungmo@gmail.com>
2020-10-07 08:25:26 +0900

    챗봇 업데이트 13 (챗봇 운영 datalog.xlsx 업데이트 20201007 - 중간고사 날)

--- git_update.sh
+++ git_update.sh
@@ -3,5 +3,5 @@
 echo "git 업데이트"
 
 git add .
-git commit -m "챗봇 업데이트 13 (챗봇 운영 datalog.xlsx 업데이트 20201006)"
+git commit -m "챗봇 업데이트 13 (챗봇 운영 datalog.xlsx 업데이트 20201007 - 중간고사 날)"
 git push origin master
