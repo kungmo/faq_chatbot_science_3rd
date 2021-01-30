7f1eebd7b133465a6eb1c453397637205a35e712
kungmo <kungmo@gmail.com>
2021-01-30 17:01:52 +0900

    테스트 중 20210130 (답변 개행 2 <br/>)

--- addresses/faq_chatbot.py
+++ addresses/faq_chatbot.py
@@ -85,6 +85,6 @@ def faq_answer(input):
         if result[i][1] < 0.6:
             return '입력한 질문에 대한 가장 유사한 질문의 유사도가 {:0.1f}%라서 60% 미만이라 엉뚱한 소리를 할 것 같으니 결과를 출력하지 않을게요. 질문을 더 구체적으로 써 주세요.'.format(result[i][1] * 100)
         else:
-            return '입력한 질문과의 유사도: {:0.1f}% | 질문: '.format(result[i][1] * 100) + df2['질문'][result[i][0]] + '<br/>답변: ' + df2['답변'][result[i][0]]
+            return '입력한 질문과의 유사도: {:0.1f}%<br/>질문: '.format(result[i][1] * 100) + df2['질문'][result[i][0]] + '<br/><br/>답변: ' + df2['답변'][result[i][0]]
 
 print('챗봇 불러오기 완료')
