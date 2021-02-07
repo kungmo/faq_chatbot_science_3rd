1ae9f06bf4f305e353509c7bac2cf0b4f4069459
kungmo <kungmo@gmail.com>
2021-02-07 19:11:46 +0900

    챗봇 업데이트 16 (질문 입력하는 곳에 placeholder 설명 넣기 2021204)

--- data/ebs_crawler_chrome.py
+++ data/ebs_crawler_chrome.py
@@ -4,12 +4,13 @@ import pandas as pd
 import time
 
 # Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
-driver = webdriver.Chrome('C:/WinPython37F/notebooks/faq_chatbot_science_3rd/data/chromedriver.exe')
+# driver = webdriver.Chrome('C:/WinPython37F/notebooks/faq_chatbot_science_3rd/data/chromedriver.exe') # Windows에서 사용
+driver = webdriver.Chrome('/Users/kungmo/Downloads/chromedriver') # 맥에서 사용
 # Mac 경로: /Users/kungmo/Downloads/chromedriver
 # Linux에서 Firefox 사용:
 # driver = webdriver.Firefox(executable_path='/home/kungmo/다운로드/geckodriver')
 #driver.implicitly_wait(10) # 5초 대기
-page_num = 4 # 10개 질문 모두 답변이 들어 있는 첫 번째 페이지 번호
+page_num = 2 # 10개 질문 모두 답변이 들어 있는 첫 번째 페이지 번호
 last_page_num = 80 # EBS 데이터 모을 마지막 페이지 번호
 qna_num = 0 #질문답변 세트 번호인 qna_num 초기화
 df2 = pd.DataFrame(columns=['질문', '답변']) # qnas에서 질문에 해당하는 답변을 한 세트로 묶어 df2에 저장하려고 빈 데이터프레임을 만든다.
@@ -44,6 +45,6 @@ df2.dropna(axis=0)
 driver.quit()
 #print(df2)
 
-file_name = 'df2_20200929_EBS_보충'
+file_name = 'df2_20210207_EBS_보충'
 df2.to_excel('{}.xlsx'.format(file_name))
 print('{}.xlsx'.format(file_name))
