cc587c65c2bd5ee527c5d17f7138dcf5dbce76cc
kungmo <kungmo@gmail.com>
2021-02-17 21:47:09 +0900

    챗봇 업데이트 17 (다중접속 가혹조건 테스트. 임시로 datalog_heavytest.xlsx 사용. 20210217)

--- addresses/faq_chatbot.py
+++ addresses/faq_chatbot.py
@@ -32,15 +32,18 @@ filter_mecab = ['NNG',  # 보통명사
                 'SN'  # 숫자 추가함 (20200929)
                 ]
 
+
 def tokenize_mecab(doc):
     # jpype.attachThreadToJVM()
     token_doc = ['/'.join(word) for word in mecab.pos(doc)]
     return token_doc
 
+
 def tokenize_mecab_noun(doc):
     # jpype.attachThreadToJVM()
     token_doc = ['/'.join(word) for word in mecab.pos(doc) if word[1] in filter_mecab]
     return token_doc
+
 
 index_questions = []
 
@@ -49,6 +52,7 @@ for i in range(len(df2)):  # df2가 0부터 시작
 
 # Doc2Vec에서 사용하는 태그문서형으로 변경
 tagged_questions = [TaggedDocument(d, [int(c)]) for d, c in index_questions]
+
 
 # FAQ 답변
 def faq_answer(input, useragent):
@@ -63,10 +67,11 @@ def faq_answer(input, useragent):
         result = d2v_faqs.docvecs.most_similar([test_vector], topn=topn)
 
         for i in range(topn):
-            print("유사질문 {}위 | 유사도: {:0.3f} | 문장 번호: {} | {}".format(i + 1, result[i][1], result[i][0], df2['질문'][result[i][0]]))
-            #print("\t질문 {} | 문장 번호: {} | {}".format(i + 1, result[i][0], df2['답변'][result[i][0]]))
-            #print(len(df2['답변'][result[i][0]]))
-            #for j in range(len(df2['답변'][result[i][0]])):
+            print("유사질문 {}위 | 유사도: {:0.3f} | 문장 번호: {} | {}".format(i + 1, result[i][1], result[i][0],
+                                                                    df2['질문'][result[i][0]]))
+            # print("\t질문 {} | 문장 번호: {} | {}".format(i + 1, result[i][0], df2['답변'][result[i][0]]))
+            # print(len(df2['답변'][result[i][0]]))
+            # for j in range(len(df2['답변'][result[i][0]])):
             #    #print(j)
             #    print("\t질문 {} | 답글순서 {} | 문장 번호: {} | {}".format(i + 1, j + 1, result[i][0], df2['답변'][result[i][0]][j]))
 
@@ -75,14 +80,17 @@ def faq_answer(input, useragent):
         nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
         load_wb = openpyxl.load_workbook('/home/ubuntu/faq_chatbot_science_3rd/data/datalog.xlsx', data_only=True)
         load_ws = load_wb['Sheet']
-        time_and_input_output = [nowDatetime, useragent, result[i][1], input, df2['질문'][result[i][0]], df2['답변'][result[i][0]]]
+        time_and_input_output = [nowDatetime, useragent, result[i][1], input, df2['질문'][result[i][0]],
+                                 df2['답변'][result[i][0]]]
         # 질문이 입력된 시각, 유사도, 질문 내용, 가장 유사한 질문, 답변을 저장
-        load_ws.append(time_and_input_output) # 엑셀 파일에 차곡차곡 누가기록
+        load_ws.append(time_and_input_output)  # 엑셀 파일에 차곡차곡 누가기록
         load_wb.save('/home/ubuntu/faq_chatbot_science_3rd/data/datalog_heavytest.xlsx')
 
         if result[i][1] < 0.6:
-            return '입력한 질문에 대한 가장 유사한 질문의 유사도가 {:0.1f}%라서 60% 미만이라 엉뚱한 소리를 할 것 같으니 결과를 출력하지 않을게요. 질문을 더 구체적으로 써 주세요.'.format(result[i][1] * 100)
+            return '입력한 질문에 대한 가장 유사한 질문의 유사도가 {:0.1f}%라서 60% 미만이라 엉뚱한 소리를 할 것 같으니 결과를 출력하지 않을게요. 질문을 더 구체적으로 써 주세요.'.format(
+                result[i][1] * 100)
         else:
-            return '입력한 질문과의 유사도: {:0.1f}%<br/><br/>질문: '.format(result[i][1] * 100) + df2['질문'][result[i][0]] + '<br/><br/>답변: ' + df2['답변'][result[i][0]]
+            return '입력한 질문과의 유사도: {:0.1f}%<br/><br/>질문: '.format(result[i][1] * 100) + df2['질문'][
+                result[i][0]] + '<br/><br/>답변: ' + df2['답변'][result[i][0]]
 
 print('챗봇 불러오기 완료')
