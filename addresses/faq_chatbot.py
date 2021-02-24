c1a643cd128afa6787479c9d20f9d92cba7f81c4
kungmo <kungmo@gmail.com>
2021-02-24 23:33:46 +0900

    챗봇 업데이트 18 (mariadb 연동 20210224)

--- addresses/faq_chatbot.py
+++ addresses/faq_chatbot.py
@@ -89,7 +89,7 @@ def faq_answer(input, useragent):
         conn = pymysql.connect(host='localhost', user='test', password='3014', db='chatbot_datalog', charset='utf8')
         cur = conn.cursor()
         sql = """INSERT INTO datalog (useragent, similarity, student_question, dataset_question, answer) VALUES (%s, %f, %s, %s, %s)"""
-        cur.execute(sql, ('test', 0.466, 'test', 'test', 'test')
+        cur.execute(sql, ('test', 0.466, 'test', 'test', 'test'))
         conn.commit()
         conn.close()
 
