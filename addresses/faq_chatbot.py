fb29dff65cd57198b568bbb37f6e735b579e18d2
kungmo <kungmo@gmail.com>
2021-02-25 00:01:37 +0900

    챗봇 업데이트 18 (mariadb 연동 20210224)

--- addresses/faq_chatbot.py
+++ addresses/faq_chatbot.py
@@ -88,10 +88,14 @@ def faq_answer(input, useragent):
         # 데이터베이스에 저장
         conn = pymysql.connect(host='localhost', user='test', password='3014', db='chatbot_datalog', charset='utf8mb4')
         cur = conn.cursor()
-        sql = "INSERT INTO datalog (useragent, similarity, student_question, dataset_question, answer) VALUES (%s, %f, %s, %s, %s)"
+        #sql = "INSERT INTO datalog (useragent, similarity, student_question, dataset_question, answer) VALUES (%s, %f, %s, %s, %s)"
         #val = (useragent, result[i][1], input, df2['질문'][result[i][0]], df2['답변'][result[i][0]])
-        val = ('test', 0.444, 'test', 'test', 'test')
-        cur.execute(sql, val)
+        #val = ('test', 0.444, 'test', 'test', 'test')
+        #cur.execute(sql, val)
+        sql = "INSERT INTO datalog (useragent, similarity, student_question, dataset_question, answer)  \
+               VALUES (%s, %f, %s, %s, %s)" % \
+               ("'"+useragent+"'", "'"+result[i][1]+"'", "'"+input+"'", "'"+df2['질문'][result[i][0]]+"'", "'"+df2['답변'][result[i][0]]+"'")
+        cur.execute(sql)
         conn.commit()
         conn.close()
 
