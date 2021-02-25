1ff9e594761f94529657a3f41c845ca15c679018
kungmo <kungmo@gmail.com>
2021-02-25 23:29:55 +0900

    챗봇 업데이트 18 (mariadb 연동 테스트 중 20210225)

--- addresses/faq_chatbot.py
+++ addresses/faq_chatbot.py
@@ -92,7 +92,7 @@ def faq_answer(input, useragent):
         #val = (useragent, result[i][1], input, df2['질문'][result[i][0]], df2['답변'][result[i][0]])
         #val = ('test', 0.444, 'test', 'test', 'test')
         #cur.execute(sql, val)
-        sql = """INSERT INTO datalog (useragent, similarity, student_question, dataset_question, answer) VALUES ("test", 0.432, "test", "test", "test")"""
+        sql = """INSERT INTO datalog (useragent, similarity, student_question, dataset_question, answer) VALUES ("테스트", 0.432, "테스트", "테스트", "테스트")"""
         cur.execute(sql)
         conn.commit()
         conn.close()
