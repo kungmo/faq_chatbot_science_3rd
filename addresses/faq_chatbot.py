e7c83ca68d4fc5c89fe7d77e27236af573ab8e2d
kungmo <kungmo@gmail.com>
2021-02-27 23:08:39 +0900

    챗봇 업데이트 20 (사용자 ip 알아내기 테스트 20210227)

--- addresses/faq_chatbot.py
+++ addresses/faq_chatbot.py
@@ -88,8 +88,8 @@ def faq_answer(input, useragent, client_ip):
             # 질문 입력 시 정보를 데이터베이스에 저장
             connection = pymysql.connect(host='localhost', user='test', password='3014', db='chatbot_datalog', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
             with connection.cursor() as cursor:
-                sql = """INSERT INTO datalog (useragent, similarity, student_question, dataset_question, answer, client_ip)
-                         VALUES ('%s', '%f', '%s', '%s', '%s', '%s')"""%(useragent, result[i][1], input, df2['질문'][result[i][0]], df2['답변'][result[i][0]], client_ip)
+                sql = """INSERT INTO datalog (client_ip, useragent, similarity, student_question, dataset_question, answer)
+                         VALUES ('%s', '%s', '%f', '%s', '%s', '%s')"""%(client_ip, useragent, result[i][1], input, df2['질문'][result[i][0]], df2['답변'][result[i][0]])
                 cursor.execute(sql)
             connection.commit()
             connection.close()
