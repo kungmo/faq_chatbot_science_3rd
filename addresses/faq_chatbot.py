35418e979cda4c637b00a28159893b47058366bb
kungmo <kungmo@gmail.com>
2021-02-27 20:37:03 +0900

    챗봇 업데이트 18 (mariadb 연동 테스트 중 20210227)

--- addresses/faq_chatbot.py
+++ addresses/faq_chatbot.py
@@ -90,7 +90,7 @@ def faq_answer(input, useragent):
         try:
             with connection.cursor() as cursor:
                 sql = """INSERT INTO datalog (useragent, similarity, student_question, dataset_question, answer)
-                         VALUES ('%s', '%f', 'test3', 'test3', 'test3')"""%(useragent, result[i][1]))
+                         VALUES ('%s', '%f', 'test3', 'test3', 'test3')"""%(useragent, result[i][1])
                 print('useragent=%s' % (useragent))
                 print('result[i][1]=%f' % (result[i][1]))
                 print('sql 통과')
